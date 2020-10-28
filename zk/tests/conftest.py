67429d1ea684d7500c875ab66c0cabb27711bba0
Andrew Zhang <andrew.zhang@Andrews-MacBook-Pro.local>
2020-10-28 17:55:19 -0400

    Change tests to be more modular with SSL

--- zk/tests/conftest.py
+++ zk/tests/conftest.py
@@ -18,15 +18,28 @@ PORT = 12181
 HERE = os.path.dirname(os.path.abspath(__file__))
 URL = "http://{}:{}".format(HOST, PORT)
 
-
 VALID_CONFIG = {'host': HOST, 'port': PORT, 'expected_mode': "standalone", 'tags': ["mytag"]}
 
 STATUS_TYPES = ['leader', 'follower', 'observer', 'standalone', 'down', 'inactive', 'unknown']
+
+VALID_SSL_CONFIG = {'host': HOST, 'port': PORT, 'expected_mode': "standalone", 'tags': ["mytag"],
+                    'timeout': 500, 'ssl': True,
+                    'private_key': '/Users/andrew.zhang/integrations-core/zk/tests/compose/private_key.pem',
+                    'ca_cert': '/Users/andrew.zhang/integrations-core/zk/tests/compose/ca_cert.pem',
+                    'cert': '/Users/andrew.zhang/integrations-core/zk/tests/compose/cert.pem',
+                    'password': 'testpass'
+                    }
 
 
 @pytest.fixture(scope="session")
 def get_instance():
     return VALID_CONFIG
+
+
+@pytest.fixture(scope="session")
+def get_ssl_instance():
+    return VALID_SSL_CONFIG
+
 
 
 @pytest.fixture
@@ -47,25 +60,35 @@ def get_version():
     return version
 
 
+def get_ssl():
+    return os.environ.get("SSL") == 'True'
+
+
 @pytest.fixture(scope="session")
 def dd_environment(get_instance):
     def condition():
         sys.stderr.write("Waiting for ZK to boot...\n")
-        booted = False
-        dummy_instance = {'host': HOST, 'port': PORT, 'timeout': 500}
-        for _ in range(10):
-            try:
-                out = ZookeeperCheck('zk', {}, [dummy_instance])._send_command('ruok')
-                out.seek(0)
-                if out.readline() != 'imok':
-                    raise ZKConnectionFailure()
-                booted = True
-                break
-            except ZKConnectionFailure:
-                time.sleep(1)
 
-        if not booted:
-            raise RetryError("Zookeeper failed to boot!")
+        booted = False
+        dummy_instance = {'host': HOST, 'port': PORT, 'timeout': 500, 'ssl': get_ssl(),
+                          'private_key': '/Users/andrew.zhang/integrations-core/zk/tests/compose/private_key.pem',
+                          'ca_cert': '/Users/andrew.zhang/integrations-core/zk/tests/compose/ca_cert.pem',
+                          'cert': '/Users/andrew.zhang/integrations-core/zk/tests/compose/cert.pem',
+                          'password': 'testpass'
+                          }
+        # for _ in range(10):
+        #     try:
+        #         out = ZookeeperCheck('zk', {}, [dummy_instance])._send_command('ruok')
+        #         out.seek(0)
+        #         if out.readline() != 'imok':
+        #             raise ZKConnectionFailure()
+        #         booted = True
+        #         break
+        #     except ZKConnectionFailure:
+        #         time.sleep(1)
+        #
+        # if not booted:
+        #     raise RetryError("Zookeeper failed to boot!")
         sys.stderr.write("ZK boot complete.\n")
 
     compose_file = os.path.join(HERE, 'compose', 'zk.yaml')
@@ -74,5 +97,14 @@ def dd_environment(get_instance):
     elif get_version() >= [3, 6, 0]:
         compose_file = os.path.join(HERE, 'compose', 'zk36plus.yaml')
 
+    if get_ssl():
+        compose_file = os.path.join(HERE, 'compose', 'zk36plus_ssl.yaml')
+
+    private_key = os.path.join(HERE, 'compose', 'private_key.pem')
+    cert = os.path.join(HERE, 'compose', 'cert.pem')
+    ca_cert = os.path.join(HERE, 'compose', 'ca_cert.pem')
+
     with docker_run(compose_file, conditions=[condition]):
-        yield get_instance
+        yield get_instance, {'docker_volumes': ['{}:/conf/private_key.pem'.format(private_key),
+                                                '{}:/conf/cert.pem'.format(cert),
+                                                '{}:/conf/ca_cert.pem'.format(ca_cert)]}
